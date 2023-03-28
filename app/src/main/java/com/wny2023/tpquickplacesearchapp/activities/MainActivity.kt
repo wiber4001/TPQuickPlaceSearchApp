e0c02d27cd9ced1913c6e43d7d20b890225b3d5f
wny2023 <wiber4001@gmail.com>
2023-03-28 10:28:25 +0900

    package structure edit completed.

--- app/src/main/java/com/wny2023/tpquickplacesearchapp/activities/MainActivity.kt
+++ app/src/main/java/com/wny2023/tpquickplacesearchapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.wny2023.tpquickplacesearchapp
+package com.wny2023.tpquickplacesearchapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wny2023.tpquickplacesearchapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
