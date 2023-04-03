d1c64cdddae2790e709cf1e2cf5abf95bac98d30
wny2023 <wiber4001@gmail.com>
2023-04-03 11:00:10 +0900

    main화면에 toolbar 추가

--- app/src/main/java/com/wny2023/tpquickplacesearchapp/activities/MainActivity.kt
+++ app/src/main/java/com/wny2023/tpquickplacesearchapp/activities/MainActivity.kt
@@ -2,6 +2,9 @@ package com.wny2023.tpquickplacesearchapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.wny2023.tpquickplacesearchapp.R
 import com.wny2023.tpquickplacesearchapp.databinding.ActivityMainBinding
 
@@ -12,5 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         //작업
+        // toolbar를 제목줄로 대체 - 옵션메뉴를 가지도록
+        setSupportActionBar(binding.toolbar)
+    }//onCreate()..
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option,menu)
+        return super.onCreateOptionsMenu(menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_aa-> Toast.makeText(this, "menu_aa", Toast.LENGTH_SHORT).show()
+            R.id.menu_bb-> Toast.makeText(this, "menu_bb", Toast.LENGTH_SHORT).show()
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
